926b662a4cc996e79502659b6fab18ec26811666
Dave Knapik <daveknapik@gmail.com>
2011-11-27 18:12:00 -0500

    Added a FacebookUserController test to ensure users are not created more than once in the database

--- spec/controllers/facebook_users_controller_spec.rb
+++ spec/controllers/facebook_users_controller_spec.rb
@@ -11,18 +11,26 @@ describe FacebookUsersController do
       before(:each) do
         @graph.stub(:get_object).and_return({"id"=>"528995148", "name"=>"Dave Knapik", "first_name"=>"Dave", "last_name"=>"Knapik", "link"=>"http://www.facebook.com/daveknapik", "username"=>"daveknapik", "gender"=>"male", "timezone"=>-5, "locale"=>"en_GB", "verified"=>true, "updated_time"=>"2011-10-25T11:28:52+0000"})
         controller.stub(:get_graph).and_return(@graph)
-        get :search, :facebook_user => {:name => "daveknapik"}
       end
       
       it "should assign facebook_user" do
+        get :search, :facebook_user => {:name => "daveknapik"}
         assigns(:facebook_user).username.should == "daveknapik"
         assigns(:facebook_user).name.should == "Dave Knapik"
       end
 
       it "should save the facebook_user to the database if it isn't already there" do
+        get :search, :facebook_user => {:name => "daveknapik"}
         facebook_user = FacebookUser.find_by_username(assigns(:facebook_user).username)
         facebook_user.should_not be_nil
         facebook_user.should be_a FacebookUser
+      end
+
+      it "should not save the facebook_user to the database if it already exists" do
+        #Factory.create(:facebook_user, :name => "daveknapik")
+        get :search, :facebook_user => {:name => "daveknapik"}
+        get :search, :facebook_user => {:name => "daveknapik"}
+        FacebookUser.all.count.should == 1
       end
     end
 
