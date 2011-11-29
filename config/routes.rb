3e00b634a918b46d7b833eccdd98f2426f1caa51
Dave Knapik <daveknapik@gmail.com>
2011-11-28 20:39:47 -0500

    Updated routes.rb to allow for Facebook usernames containing dots

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 FacebookPostAggregator::Application.routes.draw do
   root :to => 'facebook_users#index'
-  
-  resources :facebook_users do
+
+  resources :facebook_users, :constraints => { :id => /[^\/]+/ } do
     get 'search', :on => :collection
     resources :posts
   end
