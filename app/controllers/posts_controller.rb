8f9b4ffce22e1107d64008b2372e160669697128
Dave Knapik <daveknapik@gmail.com>
2011-11-29 01:22:50 -0500

    Styling of posts index and user search and show pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,30 +3,28 @@ class PostsController < ApplicationController
     @graph = get_graph
     begin
       @facebook_user = FacebookUser.find_on_facebook(@graph,params[:facebook_user_id])
+
+      if @facebook_user.posts.count > 0
+        posts = @graph.get_connections(@facebook_user.fb_id,'posts', :since => @facebook_user.posts.order("fb_created_at DESC").first.fb_created_at.to_i)
+      else
+        posts = @graph.get_connections(@facebook_user.fb_id,'posts')
+      end 
+      
+      posts.each do |p|
+        post = @facebook_user.posts.build(:title => p['message'] ? p['message'] : p['story'],
+                                          :comment_count => p['comments']['count'],
+                                          :fb_id => p['id'].split('_')[1],
+                                          :fb_created_at => p['created_time'])
+        post.save
+      end
     rescue Koala::Facebook::APIError => e
       if e.message.include? "expired"
-        flash[:error] = 'Your session had expired. Please try your last search again now.'
+        flash[:error] = 'Your session has expired. Please try your last search again using the box below.'
         redirect_to oauth.url_for_oauth_code(:permissions => :read_stream)
       else
         flash[:error] = 'We were unable to find a user by the name of "' + params[:facebook_user_id] + '"'
         redirect_to facebook_users_path
       end
-    end
-    
-    if @facebook_user.posts.count > 0
-      #get posts since last import and update posts table with these posts
-      #, :since => @facebook_user.posts.first.created_at.to_i
-      posts = @graph.get_connections(@facebook_user.fb_id,'posts', :since => @facebook_user.posts.order("fb_created_at DESC").first.fb_created_at.to_i)
-    else
-      posts = @graph.get_connections(@facebook_user.fb_id,'posts')
-    end 
-    
-    posts.each do |p|
-      post = @facebook_user.posts.build(:title => p['message'] ? p['message'] : p['story'],
-                                        :comment_count => p['comments']['count'],
-                                        :fb_id => p['id'].split('_')[1],
-                                        :fb_created_at => p['created_time'])
-      post.save
     end
   end
 end
--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -3,11 +3,12 @@ Given /^there is a Facebook user with posts$/ do
 end
 
 When /^I go to that Facebook user's posts index page$/ do
+  FacebookUser.stub(:find_on_facebook).and_return(@facebook_user)
   visit facebook_user_posts_path(@facebook_user.username)
 end
 
 Then /^I should see a list of posts$/ do
   @facebook_user.posts.each do |post|
-    page.should have_link(post.title, :href => "http://www.facebook.com/#{@facebook_user.username}/posts/#{post.fb_id}")
+    page.should have_content post.title
   end
 end
